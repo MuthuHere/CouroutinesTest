9ee8609a02b2f829b7bca3ccc781218ee1a1df37
MuthuSubramaniam <39517706+MuthuSubramaniam@users.noreply.github.com>
2022-10-09 18:37:37 +0800

    1 suspend function init

--- app/src/main/java/com/muthu/couroutinestest/MainActivity.kt
+++ app/src/main/java/com/muthu/couroutinestest/MainActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,11 +27,29 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+        //challenge
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.i("MMM a", Thread.currentThread().name)
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.i("MMM b", Thread.currentThread().name)
+        }
     }
 
-    private fun download() {
+    /**
+     * using suspend function
+     * to use withContext(which is used to change thread)
+     * in IO thread we cannot use view
+     * so changing IO thread to Main thread
+     * for that we are using withContext function also if we need to use withContext
+     * we must declare the function is suspend
+     */
+    private suspend fun download() {
         for (i in 1..100000) {
-            Log.i("MMM ", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                textView.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
         }
     }
 }
